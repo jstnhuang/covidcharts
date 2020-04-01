92c0a876ff25dd1de44a145626b38bb5ef8f907c
Justin Huang <jstn@cs.washington.edu>
2020-04-01 00:01:43 -0700

    Add code to source Python environment automatically

--- update_data.bash
+++ update_data.bash
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+source venv/bin/activate
 pushd backend
 ./download_raw_data.py
 ./process_data.py
 popd
 mkdir -p frontend/data
 cp backend/output/* frontend/data
+deactivate
